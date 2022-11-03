5990748ad96650762de81e4e1b56491300b9bfb8
Manuel-Smith <riddicksmith37@gmail.com>
2022-11-03 19:27:27 +0300

    The method working now

--- oproto.js
+++ oproto.js
@@ -1,18 +1,19 @@
+// creating myArray function for the prototype.
+
 Array.prototype.myMap = function(callback){
     let newArr = [];
 
-    for (let i; i < this.length; i++){
-        newArr.push(callback(this[i]))
+    for (let i = 0; i < this.length; i++){
+        newArr.push(callback(this[i]));
     }
-
     return newArr;
 }
 
 
-let myArray = [0, 3, 4, 2]
+let myArray = [3, 8, 1, 3]
 
 let s_array = myArray.myMap(item=>{
-    return item * 2;
+    return item + 2;
 });
 
 console.log(s_array);
